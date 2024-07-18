acf69806b66ec32e748cc0ed3c40c10fb7c51d12
DST <abdelilah@gmail.com>
2024-07-18 08:28:19 -0500

    added jwt encoding and decoding checking user authorization for protected views with permessions

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,12 +1,21 @@
 import jwt
 from datetime import datetime, timedelta
 from django.conf import settings
+from .models import CustomUser
+
+def checkUserPayload(payload):
+    user = CustomUser.objects.get(username=payload['username']);
+    if user is not None:
+        if user.id == payload['id'] and user.password == payload['password']:
+            return 200
+    return 401
 
 def generate_jwt(user):
     payload = {
-        'exp': datetime.utcnow() + timedelta(days=1),
+        'exp': datetime.utcnow() + timedelta(days=4),
         'iat': datetime.utcnow(),
         'username':user.username,
+        'id': user.id,
         'password': user.password,
     }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from .models import CustomUser
-from .utils import generate_jwt, decode_jwt
+from .utils import generate_jwt, decode_jwt, checkUserPayload
 from functools import wraps
 import json
 
@@ -11,44 +11,55 @@ def jwt_required(func):
     def wrapper_function(request, *args, **kwargs):
         token = request.META.get('HTTP_AUTHORIZATION')
         if token:
-            token = token.split(' ')[1]
             payload = decode_jwt(token)
             if payload:
-                return func(request, *args, **kwargs)
+                if checkUserPayload(payload) == 200:
+                    request.user = payload
+                    return func(request, *args, **kwargs)
         return JsonResponse({'error': 'Unauthorized'}, status=401)
 
     return wrapper_function
 
-@jwt_required
 def login(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         if data['username'] and data['password']:
-            print(data);
             user = authenticate(username=data['username'], password=data['password'])
             if user is not None:
-                print(f"user : {user}")
-                return JsonResponse({'message' : 'login success', 'status':200})
+                token = generate_jwt(user)
+                return JsonResponse({'message': 'login success', 'token': token}, status= 200)
+            return JsonResponse({'error': 'bad credential',}, status=401)
     return HttpResponse('bad http request')
 
 def register(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        if data['username'] and data['fullname'] and data['email'] and data['password']:
+        if (data['username'] and 
+            data['fullname'] and 
+            data['email'] and 
+            data['password']):
             user = CustomUser.objects.create_user(username=data['username'], 
-                                                full_name=data['fullname'], 
-                                                email=data['email'], 
-                                                password=data['password'])
+                                                    full_name=data['fullname'], 
+                                                    email=data['email'], 
+                                                    password=data['password'])
             if user is not None:
-                token = generate_jwt(user)
-                return JsonResponse({'message' : 'user registred succefully', 'status':200, 'token':token})
+                return JsonResponse({'message' : 'user registred succefully'}, status=200)
+        return HttpResponse('somthin went wrong');
     return HttpResponse('bad http request')
 
 @jwt_required
 def changePassword(request):
     if request.method == 'POST':
-        print(f"request user : {request.user}")
-        if request.user.is_authenticated:
-            print(f"request user : {request.user}")
-        return HttpResponse('done')
+        data = json.loads(request.body)
+        if data['newpassword']:
+            user = CustomUser.objects.get(id=request.user['id'])
+            if user is not None:
+                print(f"new password : {data['newpassword']}")
+                user.set_password(data['newpassword'])
+                user.save()
+        return HttpResponse(status=200)
     return HttpResponse('bad http request')
+
+@jwt_required
+def updateInfo(request):
+    pass
